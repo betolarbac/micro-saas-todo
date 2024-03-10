8ab417a17b3c9c33fd0b02a0bf1448734c8ed172
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-03-10 20:31:44 -0300

    feat: pagina de bem vindo

---
+++ src/app/app/_components/user-info.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+
+
+
+type props = {
+  user: Session['user']
+
+}
+
+export function UserInfo({ user }: props) {
+
+  if (!user) return
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <Avatar>
+        <AvatarFallback>U</AvatarFallback>
+      </Avatar>
+
+      <span>{user.email}</span>
+
+      <Button variant="outline" onClick={() => signOut()}>Sign out</Button>
+    </div>
+  );
+}
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>app page </h1>
+
+import { auth } from "@/services/auth";
+import { UserInfo } from "./_components/user-info";
+
+export default async function Page() {
+  const session = await auth()
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <UserInfo user={session?.user}/>
+    </main>
+  );
 }
