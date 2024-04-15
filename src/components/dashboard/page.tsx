226dbb1769b17c8105f7c2e5bbde0672ad36ecf9
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-04-14 23:54:42 -0300

    feat: dashboardPage  usando Composition Pattern adicionado ao projeto

---
+++ src/components/dashboard/page.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+
+export type DashboardPagerGenericProps<T = unknown> = {
+  children: React.ReactNode;
+  className?: string;
+} & T;
+
+export function DashboardPage({ children, className }: DashboardPagerGenericProps) {
+  return (
+    <section className={cn(["h-screen", className])}>
+      {children}
+    </section>
+  );
+}
+
+export function DashboardPageHeader({ children, className }: DashboardPagerGenericProps) {
+  return (
+    <header className={cn(["px-6 py-3 border-b border-border", className])}>
+      {children}
+    </header>
+  );
+}
+
+export function DashboardPageHeaderTitle({ children, className }: DashboardPagerGenericProps) {
+  return (
+    <h1 className={cn(["text-muted-foreground uppercase", className])}>
+      {children}
+    </h1>
+  );
+}
+
+export function DashboardPageHeaderNav({ children, className }: DashboardPagerGenericProps) {
+  return (
+    <nav className={cn(["", className])}>
+      {children}
+    </nav>
+  );
+}
+
+export function DashboardPageMain({ children, className }: DashboardPagerGenericProps) {
+  return (
+    <main className={cn(["p-6", className])}>
+      {children}
+    </main>
+  );
+}
