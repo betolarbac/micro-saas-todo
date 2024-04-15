836119bbfd93a47505bcc385226ffdf0f651eb9f
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-04-15 00:12:34 -0300

    feat: função de user adicionado

--- src/app/app/_components/user-dropdown.tsx
+++ src/app/app/_components/user-dropdown.tsx
@@ -14,21 +14,31 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LockClosedIcon, MixerVerticalIcon, RocketIcon } from "@radix-ui/react-icons"
+import { Session } from "next-auth"
+import { signOut } from "next-auth/react"
 
-export function UserDropdown() {
+type UserDropdownProps = {
+  user: Session['user']
+
+}
+
+export function UserDropdown({user}: UserDropdownProps) {
+
+  if(!user) return
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="link" className="relative h-8 flex items-center justify-between space-x-2 w-full !px-0">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src={user.image as string} alt={user.name as string} />
+            <AvatarFallback>U</AvatarFallback>
           </Avatar>
 
           <div className="flex flex-col flex-1 space-y-1 text-left">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            {user.name && (<p className="text-sm font-medium leading-none">{user.name}</p>)}
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </Button>
@@ -36,9 +46,9 @@ export function UserDropdown() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">{user.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -54,7 +64,7 @@ export function UserDropdown() {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()}>
         <LockClosedIcon className="w-3 h-3 mr-3" />
           Log out
         </DropdownMenuItem>
