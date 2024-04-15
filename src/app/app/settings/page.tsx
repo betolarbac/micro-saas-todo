aa4218e6b43a22231be5764eaad22ff0062f2a5e
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-04-14 23:28:14 -0300

    feat: criação de pagina de settings

---
+++ src/app/app/settings/page.tsx
@@ -0,0 +1,13 @@
+
+import { auth } from "@/services/auth";
+
+
+export default function Page() {
+
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1>settings</h1>
+    </main>
+  );
+}
