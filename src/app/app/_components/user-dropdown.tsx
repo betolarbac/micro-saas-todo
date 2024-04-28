f4839457f332c3b72f7b239f14fb256837ad11f8
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-04-28 20:47:11 -0300

    feat: modo dark e avatar user

--- src/app/app/_components/user-dropdown.tsx
+++ src/app/app/_components/user-dropdown.tsx
@@ -1,9 +1,5 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,31 +8,44 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { LockClosedIcon, MixerVerticalIcon, RocketIcon } from "@radix-ui/react-icons"
-import { Session } from "next-auth"
-import { signOut } from "next-auth/react"
+} from "@/components/ui/dropdown-menu";
+import {
+  LockClosedIcon,
+  MixerVerticalIcon,
+  MoonIcon,
+  RocketIcon,
+  SunIcon,
+} from "@radix-ui/react-icons";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import { useTheme } from "next-themes";
 
 type UserDropdownProps = {
-  user: Session['user']
+  user: Session["user"];
+};
 
-}
+export function UserDropdown({ user }: UserDropdownProps) {
+  if (!user) return;
 
-export function UserDropdown({user}: UserDropdownProps) {
+  const {theme, setTheme} = useTheme();
 
-  if(!user) return
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="link" className="relative h-8 flex items-center justify-between space-x-2 w-full !px-0">
+        <Button
+          variant="link"
+          className="relative h-8 flex items-center justify-between space-x-2 w-full !px-0"
+        >
           <Avatar className="h-8 w-8">
             <AvatarImage src={user.image as string} alt={user.name as string} />
-            <AvatarFallback>U</AvatarFallback>
+            <AvatarFallback className="uppercase">{user.email?.charAt(0)}</AvatarFallback>
           </Avatar>
 
           <div className="flex flex-col flex-1 space-y-1 text-left">
-            {user.name && (<p className="text-sm font-medium leading-none">{user.name}</p>)}
+            {user.name && (
+              <p className="text-sm font-medium leading-none">{user.name}</p>
+            )}
             <p className="text-xs leading-none text-muted-foreground">
               {user.email}
             </p>
@@ -54,21 +63,26 @@ export function UserDropdown({user}: UserDropdownProps) {
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-          <MixerVerticalIcon className="w-3 h-3 mr-3" />
-            Configurações 
+          <DropdownMenuItem onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            <SunIcon className="w-3 h-3 mr-3  hidden dark:inline " />
+            <MoonIcon className="w-3 h-3 mr-3  dark:hidden " />
+            Toggle theme
           </DropdownMenuItem>
           <DropdownMenuItem>
-          <RocketIcon className="w-3 h-3 mr-3" />
+            <MixerVerticalIcon className="w-3 h-3 mr-3" />
+            Configurações
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <RocketIcon className="w-3 h-3 mr-3" />
             Upgrade
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={() => signOut()}>
-        <LockClosedIcon className="w-3 h-3 mr-3" />
+          <LockClosedIcon className="w-3 h-3 mr-3" />
           Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
