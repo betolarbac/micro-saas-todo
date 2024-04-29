ba96acada6b228c1752bf6c2d83c899cd4fadde1
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-04-28 21:07:07 -0300

    feat: pagina de plano

--- src/app/app/settings/billing/page.tsx
+++ src/app/app/settings/billing/page.tsx
@@ -1,6 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 
 export default function Page() {
   return (
-        <h1>Configurações / billing</h1>
+    <Card>
+      <CardHeader className="border-b border-border">
+        <CardTitle>Uso do Plano</CardTitle>
+        <CardDescription>
+          Você está atualmente no plano [current_plan]. Ciclo de cobrança atual:{''}
+          [next_due_date].
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pt-6">
+        <div className="space-y-2">
+          <header className="flex items-center justify-between">
+            <span className="text-muted-foreground text-sm">1/5</span>
+            <span className="text-muted-foreground text-sm">20%</span>
+          </header>
+
+          <main>
+            <Progress value={20} />
+          </main>
+        </div>
+      </CardContent>
+      <CardFooter className="flex items-center justify-between border-t border-border pt-6">
+        <span>Para obter um limite maior, assine o plano PRO</span>
+        <Button>Atualizar para PRO</Button>
+      </CardFooter>
+    </Card>
   );
 }
+
