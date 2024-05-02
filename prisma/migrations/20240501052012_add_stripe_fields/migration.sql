65042e202c68897fc68a4414010ffd20ca9c465b
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-05-01 22:39:11 -0300

    feat: schema de prisma e actions de checkout

---
+++ prisma/migrations/20240501052012_add_stripe_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "stripeCustomerID" TEXT;
+ALTER TABLE "User" ADD COLUMN "stripePriceId" TEXT;
+ALTER TABLE "User" ADD COLUMN "stripeSubscriptionID" TEXT;
+ALTER TABLE "User" ADD COLUMN "stripeSubscriptionStatus" TEXT;
